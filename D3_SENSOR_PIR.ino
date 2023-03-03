0757efe458720c8d6bdfe8ec3901d32a829539d4
omaruli2002 <95552932+omaruli2002@users.noreply.github.com>
2023-03-03 14:40:29 -0600

    Update D3_SENSOR_PIR.ino

--- D3_SENSOR_PIR.ino
+++ D3_SENSOR_PIR.ino
@@ -3,45 +3,6 @@ MEDIR TEMPERATURA Y HUMEDAD CON ARDUINO Y SENSOR DHT11-DHT22
 Código por: Ulises Chávez 
 Fecha: 01/03/2023
 */
-const int LEDPin = 13;        // pin para el LED
-const int PIRPin = 2;         // pin de entrada (for PIR sensor)
-
-int pirState = LOW;           // de inicio no hay movimiento
-int val = 0;                  // estado del pin
-
-void setup() 
-{
-   pinMode(LEDPin, OUTPUT); 
-   pinMode(PIRPin, INPUT);
-   Serial.begin(9600);
-}
-
-void loop()
-{
-   val = digitalRead(PIRPin);
-   if (val == HIGH)   //si está activado
-   { 
-      digitalWrite(LEDPin, HIGH);  //LED ON
-      if (pirState == LOW)  //si previamente estaba apagado
-      {
-        Serial.println("Sensor activado");
-        pirState = HIGH;
-      }
-   } 
-   else   //si esta desactivado
-   {
-      digitalWrite(LEDPin, LOW); // LED OFF
-      if (pirState == HIGH)  //si previamente estaba encendido
-      {
-        Serial.println("Sensor parado");
-        pirState = LOW;
-      }
-   }
-}
-
-// CODIGO  BASICO CON UN LED QUE SOLO LEE EL ESTADO Y LO ENCIENDE AL ESTAR ACTIVADO
-
-/*
 const int LEDPin= 13;
 const int PIRPin= 2;
 
@@ -67,5 +28,3 @@ void loop()
     digitalWrite(LEDPin, LOW);
   }
 }
-
-*/
