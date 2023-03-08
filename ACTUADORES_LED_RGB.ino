7e830b4deb12af3b7be1991c45771a2b8d8bff68
omaruli2002 <95552932+omaruli2002@users.noreply.github.com>
2023-03-08 17:07:44 -0600

    Update ACTUADORES_LED_RGB.ino

--- ACTUADORES_LED_RGB.ino
+++ ACTUADORES_LED_RGB.ino
@@ -1,3 +1,5 @@
+// CODIGO PARA LED RGB DE ANODO COMÚN
+
 int redPin = 6;
 int greenPin = 5;
 int bluePin = 3;
@@ -30,3 +32,43 @@ void setColor(int redValue, int greenValue, int blueValue) {
   analogWrite(greenPin, greenValue);
   analogWrite(bluePin, blueValue);
 }
+
+
+// CÓDIGO PARA LED RGB DE CÁTODO COMÚN
+
+/*
+int redPin = 6;
+int greenPin = 5;
+int bluePin = 3;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  // Colores RGB básicos
+  setColor(0, 255, 255);  // Cian
+  delay(1000);
+  setColor(255, 0, 255);  // Magenta
+  delay(1000);
+  setColor(255, 255, 0);  // Amarillo
+  delay(1000);
+  // Mezclas de colores RGB
+  setColor(255, 0, 0);  // Rojo
+  delay(1000);
+  setColor(0, 255, 0);  // Verde
+  delay(1000);
+  setColor(0, 0, 255);  // Azul
+  delay(1000);
+}
+
+void setColor(int redValue, int greenValue, int blueValue) {
+  analogWrite(redPin, 255-redValue);
+  analogWrite(greenPin, 255-greenValue);
+  analogWrite(bluePin, 255-blueValue);
+}
+
+
+*/
