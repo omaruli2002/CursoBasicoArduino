45607b72fb604d05b3e689d97b596fc9b62671c6
omaruli2002 <95552932+omaruli2002@users.noreply.github.com>
2023-03-20 19:29:08 -0600

    Create MODULOS_RELOJ.ino

---
+++ MODULOS_RELOJ.ino
@@ -0,0 +1,40 @@
+#include <virtuabotixRTC.h> //Libreria
+
+// CLK -> 6, DAT -> 7, RST -> 8
+// SCLK -> 6, I/O -> 7, CE -> 8 
+
+virtuabotixRTC myRTC(6, 7, 8); // Si cambiamos los PIN de conexión, debemos cambiar aquí tambien
+
+void setup() {
+  Serial.begin(9600);
+
+  // Para ajustar la fecha y hora, debemos utilizar el siguiente formato:
+  // segundos, minutos, horas, dia de la semana, numero de día, mes y año
+  myRTC.setDS1302Time(00, 45, 15, 7, 9, 2, 2019); // SS, MM, HH, DW, DD, MM, YYYY
+  /* después de ajustar la hora la linea de arriba se debe comentar o eliminar
+   *  para que la fecha y hora quede grabada
+  */
+  // La configuración de fecha y hora ha sido ajustada
+}
+
+void loop() {
+  // Esta función actualiza las variables para obtener resultados actuales
+  myRTC.updateTime();
+
+  // Se imprime el resultado en el Monitor Serial
+  Serial.print("Fecha y hora actual: ");
+  Serial.print(myRTC.dayofmonth); // Se puede cambiar entre día y mes si se utiliza el sistema Americano
+  Serial.print("/");
+  Serial.print(myRTC.month);
+  Serial.print("/");
+  Serial.print(myRTC.year);
+  Serial.print(" ");
+  Serial.print(myRTC.hours);
+  Serial.print(":");
+  Serial.print(myRTC.minutes);
+  Serial.print(":");
+  Serial.println(myRTC.seconds);
+
+  // Un pequeño delay para no repetir y leer más facil
+  delay(1000);
+}
