580c305c42a33b2162fbf7959f06adf57bcf7a38
omaruli2002 <95552932+omaruli2002@users.noreply.github.com>
2023-03-20 18:40:16 -0600

    Update MODULO_IR_TRANSMISIÓN.ino

--- MODULO_IR_TRANSMISIÓN.ino
+++ MODULO_IR_TRANSMISIÓN.ino
@@ -1,13 +1,68 @@
-#include <IRremote.h>
-IRsend irsend;
+/*
+ * SimpleSender.cpp
+ *
+ *  Demonstrates sending IR codes in standard format with address and command
+ *  An extended example for sending can be found as SendDemo.
+ *
+ *  Copyright (C) 2020-2022  Armin Joachimsmeyer
+ *  armin.joachimsmeyer@gmail.com
+ *
+ *  This file is part of Arduino-IRremote https://github.com/Arduino-IRremote/Arduino-IRremote.
+ *
+ *  MIT License
+ */
+#include <Arduino.h>
 
-void setup()
-{
-  Serial.begin(9600);
+#define DISABLE_CODE_FOR_RECEIVER // Disables restarting receiver after each send. Saves 450 bytes program memory and 269 bytes RAM if receiving functions are not used.
+//#define SEND_PWM_BY_TIMER         // Disable carrier PWM generation in software and use (restricted) hardware PWM.
+//#define USE_NO_SEND_PWM           // Use no carrier PWM, just simulate an active low receiver signal. Overrides SEND_PWM_BY_TIMER definition
+
+#include "PinDefinitionsAndMore.h" // Define macros for input and output pin etc.
+#include <IRremote.hpp>
+
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    Serial.begin(115200);
+
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_IRREMOTE));
+    Serial.print(F("Send IR signals at pin "));
+    Serial.println(IR_SEND_PIN);
+
+    /*
+     * The IR library setup. That's all!
+     */
+//    IrSender.begin(); // Start with IR_SEND_PIN as send pin and if NO_LED_FEEDBACK_CODE is NOT defined, enable feedback LED at default feedback LED pin
+    IrSender.begin(DISABLE_LED_FEEDBACK); // Start with IR_SEND_PIN as send pin and disable feedback LED at default feedback LED pin
 }
 
-void loop() 
-{
-  irsend.sendNEC(0x20DF10EF, 32); //Insertar aqui el codigo que queremos enviar
-  delay(5000);
+/*
+ * Set up the data to be sent.
+ * For most protocols, the data is build up with a constant 8 (or 16 byte) address
+ * and a variable 8 bit command.
+ * There are exceptions like Sony and Denon, which have 5 bit address.
+ */
+uint8_t sCommand = 0x44;
+uint8_t sRepeats = 0;
+
+void loop() {
+    /*
+     * Print current send values
+     */
+    Serial.println();
+    Serial.print(F("Send now: address=0x00, command=0x"));
+    Serial.print(sCommand, HEX);
+    Serial.print(F(", repeats="));
+    Serial.print(sRepeats);
+    Serial.println();
+
+    Serial.println(F("Send standard NEC with 8 bit address"));
+    Serial.flush();
+
+    // Receiver output for the first loop must be: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
+    IrSender.sendNEC(0xFB04, sCommand, sRepeats);
+
+
+    delay(5000);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
 }
